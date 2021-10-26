76a9925bae5360c672d16852d9d2776b10710420
Estede-tadday Makusa <estede-tadday.makusa@epitech.eu>
2021-10-26 21:33:43 +0200

    [+] problem setting

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/pjbeaugendre/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/pjbeaugendre/epitech/estede/redditech/B-DEV-501-LIL-5-1-redditech-paul.fermey/redditech"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
